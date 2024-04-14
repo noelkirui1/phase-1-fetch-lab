cb6abe5a0284e55dcc4d3f034223734da6ae4333
noelkirui1 <kiruinoel16@gmail.com>
2024-04-14 08:40:27 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error fetching books:', error));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
